031189c447d302f7ad76195197ae3580985e943a
Melvyn <melvyn.fontaine@epitech.eu>
2019-01-25 18:01:50 +0100

    matchstick full

--- SRC/my_put_nbr.c
+++ SRC/my_put_nbr.c
@@ -8,44 +8,16 @@
 #include "my.h"
 #include<unistd.h>
 
-int my_pow(int value, int exponent)
-{
-	if (exponent == 0)
-	{
-		my_putchar (1);
-	}
-
-	while (exponent > 0)
-	{
-		value = 10 * value;
-		exponent = exponent - 1;
-	}
-	return (value);
-}
-
 int my_put_nbr(int nb)
 {
-	if (nb < 0)
-	{
+	if (nb < 0) {
 		my_putchar('-');
 		nb = nb * (-1);
 	}
-	int tmp = nb;
-	int cpt = 0;
-
-	while (tmp > 0)
-	{
-		tmp = tmp / 10;
-		cpt = cpt + 1;
+	if (nb >= 10) {
+		my_put_nbr(nb / 10);
 	}
-	int mpow = my_pow(10, cpt - 2);
-	int unit = 0;
-
-	while (mpow > 0)
-	{
-		unit = nb / mpow;
-		my_putchar(unit %10 + '0');
-		mpow = mpow / 10;
-	}
+	nb = nb % 10 + '0';
+	my_putchar(nb);
 	return (0);
 }
